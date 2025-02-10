a6f84bee4e14f9ce4b0f93d486082271bb4ee962
RubenSab <rubensabatini2005@gmail.com>
2025-02-10 13:09:30 +0100

    Quartz sync: Feb 10, 2025, 1:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "🖥️  Wiki",
+    pageTitleSuffix: "Informatica Sapienza",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
